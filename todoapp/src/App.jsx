0d1b1b65dc9df7438e772ae158dd44b412a5d38c
Mikiyas <damtewmikiyas@gmali.com>
2024-07-11 16:25:13 +0300

    Added style to the heading

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import Todo from "./component/Todo";
+import Heading from "./component/Heading";
+import './App';
 
 function App() {
   return (
     <div className="App">
+      <Heading />
       <Todo />
     </div>
   );
---
+++ todoapp/src/component/Heading.jsx
@@ -0,0 +1,5 @@
+import styles from './heading.module.css';
+
+export default function Heading(){
+    return (<div className={styles.header}>My Todo List</div>);
+}
