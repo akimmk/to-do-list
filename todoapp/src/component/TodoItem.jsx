f81e2b7829c5c8e047dba2f4de3dc44b067e063b
Mikiyas <damtewmikiyas@gmali.com>
2024-07-12 09:54:15 +0300

    todo completed functionality

--- todoapp/src/component/TodoItem.jsx
+++ todoapp/src/component/TodoItem.jsx
@@ -4,10 +4,20 @@ export default function TodoItem({ item, todos, setTodos }) {
   function handleDelete() {
     setTodos(todos.filter((todo) => todo !== item));
   }
+  function handelClick(name) {
+    setTodos(
+      todos.map((todo) =>
+        todo.name === name ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  }
+  const completed = item.done ? styles.completed : "";
   return (
     <div className={styles.container}>
       <div className={styles.todoItems}>
-        {item.name}
+        <span className={completed} onClick={() => handelClick(item.name)}>
+          {item.name}
+        </span>
         <span>
           <button onClick={() => handleDelete(item)} className={styles.btn}>
             x
